3ce26c47845d14c5395dff45171de9c483245b8e
LTBurton1 <Liam.burton@hotmail.com>
2022-02-25 20:54:30 +0000

    Sort questions by ID

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -36,6 +36,8 @@ function App() {
       const data = await res.json();
       console.log("Questions:", data.payload);
 
+      data.payload.sort((a, b) => (a.question_id > b.question_id ? 1 : -1));
+
       setQuestions(data.payload);
     }
     getQuestions();
